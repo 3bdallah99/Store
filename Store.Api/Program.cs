912fd453438c5f1d766a96d65168ae3005dd8138
3bdallah99 <83489211+3bdallah99@users.noreply.github.com>
2025-04-14 12:39:53 +0200

    Prepare Specifications Classes

--- Store.Api/Program.cs
+++ Store.Api/Program.cs
@@ -47,7 +47,7 @@ namespace Store.Api
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-
+            app.UseStaticFiles();
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
